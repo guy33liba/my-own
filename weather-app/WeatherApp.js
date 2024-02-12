4c7b042e7e9fd13005a75c29194c268ae7356c3d
guy33liba <81708094+guy33liba@users.noreply.github.com>
2024-02-12 14:12:37 +0200

    Update WeatherApp.js

--- weather-app/WeatherApp.js
+++ weather-app/WeatherApp.js
@@ -2,27 +2,32 @@ import React, { useEffect, useState } from "react"
 import "./WeatherApp.css"
 import axios from "axios"
 const WeatherApp = () => {
-  const [input, setInput] = useState("")
-  const [temperture, setTemperture] = useState("")
-  const [city, setCity] = useState("")
-
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=6557810176c36fac5f0db536711a6c52`
-
-  const fetchAPI = async () => {
-    await axios.get(url).then((res) => console.log(res))
+  const [data, setdata] = useState([])
+  const [temperture, setTemperture] = useState([])
+  const [city, setCity] = useState([])
+  const [desc, setdesc] = useState([])
+  const key = "0319aa6547fb86bd61c95c4d0afe1e0a"
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${key}`
+  const fetchAPI = () => {
+    axios.get(url).then((response) => {
+      setdata(response.data)
+    })
+    setCity(data.name)
+    setdesc(data)
+    console.log(data)
   }
   useEffect(() => {
     fetchAPI()
   }, [])
   const submit = () => {
-    setCity(input)
+    setCity(data.name)
   }
   return (
     <div>
       <div>
         <div className="container">
           <img src="background.jpg" alt="" className="background" />
-          <input type="text" className="city-input" value={input} onChange={(e) => setInput(e.target.value)} />
+          <data type="text" className="city-data" value={data} onChange={(e) => setdata(e.target.value)} />
 
           <div className="weather-container">
             <div className="sun-status">
@@ -32,11 +37,11 @@ const WeatherApp = () => {
             <div className="weather-content">
               today
               {city ? <div className="city">{city}</div> : <div>city Name</div>}
-              <div className="temperture">{temperture} °C</div>
-              <div className="sky-state">clear sky</div>
+              <div className="temperture">{temperture} °F</div>
+              <div className="sky-state"> fefe{desc}</div>
               <div>
                 <button className="submit-button" onClick={() => submit()}>
-                  Submit Weather
+                  Submit Weather {city}
                 </button>
               </div>
             </div>
@@ -47,7 +52,7 @@ const WeatherApp = () => {
                   <div>
                     <img src="sun.png" alt="" className="days-image" />
                   </div>
-                  <div>17 °C</div>
+                  <div>{temperture}</div>
                 </div>
               </div>
 
@@ -57,7 +62,7 @@ const WeatherApp = () => {
                   <div>
                     <img src="cloud.png" alt="" className="days-image" />
                   </div>
-                  <div>17 °C</div>
+                  <div>{temperture} </div>
                 </div>
               </div>
 
@@ -66,7 +71,7 @@ const WeatherApp = () => {
                   FriDay
                   <div>
                     <img src="sun.png" alt="" className="days-image" />
-                    <div>17 °C</div>
+                    <div>{temperture}</div>
                   </div>
                 </div>
               </div>
@@ -76,7 +81,7 @@ const WeatherApp = () => {
                   SaturDay
                   <div>
                     <img src="cloud.png" alt="" className="days-image" />
-                    <div>17 °C</div>
+                    <div>{temperture}</div>
                   </div>
                 </div>
               </div>
