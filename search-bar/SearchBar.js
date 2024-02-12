3d99b4b838db1d863ac9b6fff8013eda19524294
guy33liba <81708094+guy33liba@users.noreply.github.com>
2024-02-12 17:27:44 +0200

    Create SearchBar.js

---
+++ search-bar/SearchBar.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react"
+import "./SearchBar.css"
+const SerachBar = () => {
+  const [input, setinput] = useState("")
+  const [newname, setNewname] = useState("")
+  const array = [
+    "Time",
+    "Past",
+    "Future",
+    "Dev",
+    "Fly",
+    "Flying",
+    "Soar",
+    "Soaring",
+    "Power",
+    "Kitty",
+    "Kitten",
+    "Zero",
+    "Memory",
+    "Trooper",
+    "XX",
+    "Bandit",
+    "Fear",
+    "Light",
+    "Glow",
+    "Tread",
+    "Deep",
+    "Head",
+    "Theme",
+    "Grand",
+    "Cool",
+    "Kid",
+    "Boy",
+    "Girl",
+    "Vortex",
+    "Paradox",
+    "Hello World",
+    "Inter Cooler",
+  ]
+
+  const found = array.filter((item) => item.toLowerCase().includes(input))
+  const generateName = () => {
+    const newGeneratedName = array[Math.floor(Math.random() * found.length)]
+    setNewname(() => newGeneratedName)
+  }
+  return (
+    <div>
+      <div className="container">
+        <div>
+          <div>
+            <input className="input" type="text" value={input} onChange={(e) => setinput(e.target.value)} />
+          </div>
+          <div className="found-list search-list">
+            {found.map((item) => {
+              return (
+                <div className="found-item">
+                  {item} <hr />
+                </div>
+              )
+            })}
+          </div>
+        </div>
+        <div className="generate">
+          <button onClick={() => generateName()}>generate</button>
+          <div className="new-name">{newname ? newname : "enter a name"}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SerachBar
