5b61f16d6fb2501fce205287484dadbdc57396d9
guy33liba <81708094+guy33liba@users.noreply.github.com>
2024-03-18 16:29:32 +0200

    Update Screen.js

--- calculator/Screen.js
+++ calculator/Screen.js
@@ -1,14 +1,22 @@
-import React, { useState } from "react"
+import React, { useRef, useState } from "react"
 
-const Screen = ({ screen }) => {
+const Screen = ({ screen, operatorClass, numbersClass }) => {
   const [first, setfirst] = useState("")
   const [second, setsecond] = useState("")
+  const [firstOrSecondNumber, setFirstOrSecondNumber] = useState(false)
+  const ref = useRef(null)
+  const handleClick = (e) => {
+    e.current = screen.numberValue
+    console.log(e)
+  }
   return (
     <div className="screen">
       {screen.map((item, i) => {
         return (
           <div key={i} className="screenNumbers">
-            {item.numberValue}
+            <div ref={ref} onClick={() => handleClick(item.id)}>
+              {item.numberValue}
+            </div>
           </div>
         )
       })}
